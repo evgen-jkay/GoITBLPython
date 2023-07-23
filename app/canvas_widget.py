309edeaab98c6a0c85ea8275bdb3ddbc0d5f2dc2
evgen-jkay <evgenlandarenko@gmail.com>
2023-07-23 16:20:31 +0300

    bag fix

--- app/canvas_widget.py
+++ app/canvas_widget.py
@@ -8,6 +8,26 @@ class CustomFigureCanvas(FigureCanvas):
         FigureCanvas.__init__(self, *args, **kwargs)
         self.setParent(None)
 
+        # Initialize a flag to track if the canvas updates are paused
+        self.updates_paused = False
+
+    def pause_updates(self):
+        # Pause canvas updates
+        self.updates_paused = True
+
+    def resume_updates(self):
+        # Resume canvas updates
+        self.updates_paused = False
+        self.draw_idle()  # Force an update to the canvas
+
+    def enterEvent(self, event):
+        # Resume canvas updates when the mouse enters the canvas widget
+        self.resume_updates()
+
+    def leaveEvent(self, event):
+        # Pause canvas updates when the mouse leaves the canvas widget
+        self.pause_updates()
+
 
 class CanvasContainer(QWidget):
     def __init__(self, canvas):
